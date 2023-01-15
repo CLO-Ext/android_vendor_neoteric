cb20aeec12ca575d304ceedee327e69ee6d03fd3
cjh1249131356 <cjh1249131356@gmail.com>
2023-01-15 17:52:32 +0800

    soong: Add TARGET_USES_NOTHING_CAMERA
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- target/board/BoardConfigSoong.mk
+++ target/board/BoardConfigSoong.mk
@@ -22,7 +22,8 @@ SOONG_CONFIG_zephyrusGlobalVars += \
     target_ld_shim_libs \
     uses_oplus_camera \
     target_process_sdk_version_override \
-    target_surfaceflinger_udfps_lib
+    target_surfaceflinger_udfps_lib \
+    uses_nothing_camera
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
@@ -35,6 +36,7 @@ SOONG_CONFIG_zephyrusGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARG
 SOONG_CONFIG_zephyrusGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_zephyrusGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
 SOONG_CONFIG_zephyrusGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
+SOONG_CONFIG_zephyrusGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 
 # Gestures
 define add-gesturevar-if-exist
