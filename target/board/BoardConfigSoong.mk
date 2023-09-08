f093a7ea245da9aa1efc49e13e0ea406e69e02ab
HELLBOY017 <abhaygill017@gmail.com>
2023-09-08 18:36:14 +0530

    vendor: Kang lineage's kernel build system
    Change-Id: I8b374887146976981d6b5701b0537efc617e206f

--- target/board/BoardConfigSoong.mk
+++ target/board/BoardConfigSoong.mk
@@ -4,6 +4,19 @@
 
 # PA Variables
 
+PATH_OVERRIDE_SOONG := $(shell echo $(TOOLS_PATH_OVERRIDE))
+
+# Add variables that we wish to make available to soong here.
+EXPORT_TO_SOONG := \
+    KERNEL_ARCH \
+    KERNEL_BUILD_OUT_PREFIX \
+    KERNEL_CROSS_COMPILE \
+    KERNEL_MAKE_CMD \
+    KERNEL_MAKE_FLAGS \
+    PATH_OVERRIDE_SOONG \
+    TARGET_KERNEL_CONFIG \
+    TARGET_KERNEL_SOURCE
+
 SOONG_CONFIG_NAMESPACES += aospaVarsPlugin
 
 SOONG_CONFIG_aospaVarsPlugin :=
